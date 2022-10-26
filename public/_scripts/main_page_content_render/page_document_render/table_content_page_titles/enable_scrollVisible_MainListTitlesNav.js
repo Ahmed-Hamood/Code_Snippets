430b39d72af8c4c3e3080f3ecceacde05d12104c
Ahmed Hamood <ahmedalqourabah@gmail.com>
2022-10-26 22:53:08 +0400

    b

--- public/_scripts/main_page_content_render/page_document_render/table_content_page_titles/enable_scrollVisible_MainListTitlesNav.js
+++ public/_scripts/main_page_content_render/page_document_render/table_content_page_titles/enable_scrollVisible_MainListTitlesNav.js
@@ -1,5 +1,10 @@
 let tablePosition = 0;
-let page_content_render = document.getElementById('page-content-render-container')
+let page_content_render = document.getElementById('page-content-render-container');
+
+const EnableScrollVisibleStickyMainListTitleNavContent = (event) => {
+ if (tablePosition <= event.target.scrollTop) document.querySelector('.titles-list-menu-active-btn')?.classList.add('view');
+ else document.querySelector('.titles-list-menu-active-btn').classList.remove('view');
+};
 
 export default function enable_scrollVisible_MainListTitlesNav(active) {
  if (active) {
@@ -10,15 +15,7 @@ export default function enable_scrollVisible_MainListTitlesNav(active) {
   }
   page_content_render.addEventListener('scroll', EnableScrollVisibleStickyMainListTitleNavContent);
  } else {
-  console.log("remove main table content scroll listener");
+  console.log('remove main table content scroll listener');
   page_content_render.removeEventListener('scroll', EnableScrollVisibleStickyMainListTitleNavContent);
  }
-}
-
-function EnableScrollVisibleStickyMainListTitleNavContent(event) {
-  if (tablePosition <= event.target.scrollTop) {
-    document.querySelector('.titles-list-menu-active-btn')?.classList.add('view');
-   } else {
-    document.querySelector('.titles-list-menu-active-btn').classList.remove('view');
-   }
 }
