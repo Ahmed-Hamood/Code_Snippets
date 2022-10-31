7b458a78b536cd0f3f24c475974b9bcef629a5d0
Ahmed Hamood <ahmedalqourabah@gmail.com>
2022-10-31 20:04:08 +0400

    implement read time calculate

--- public/_scripts/main_page_content_render/main.js
+++ public/_scripts/main_page_content_render/main.js
@@ -170,6 +170,18 @@ function startup_pageRender(isPageLoadSuccess, refPos = null) {
 
    // wrap sections after each title
    content_section_wrapper();
+
+   const text = page_content.innerText;
+   console.log(page_content.innerText);
+   const wpm = 225;
+   const words = text.trim().split(/\s+/).length;
+   let min_msg = 'minutes';
+   console.log(words / wpm);
+   const time = Math.ceil(words / wpm);
+
+   if (time == 1) min_msg = 'minute or less';
+   document.querySelector('.main-title').insertAdjacentHTML('afterend', `<span class="min-read"> ${time} ${min_msg} read time</span>`);
+
   }
  }
 }
